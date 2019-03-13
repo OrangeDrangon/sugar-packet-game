cddc12dd91588d38761ae7b8ed83192e0d2ce735
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-03-13 12:56:30 -0400

    Added function documentation

--- client/src/game.ts
+++ client/src/game.ts
@@ -1,11 +1,28 @@
 import { IRow, IMove } from './types';
 
 import { Row } from './row';
-
+/**
+ *
+ * The main class of the sugar packet game.
+ * Requires the row data, room name, and the active socket connection.
+ * It creates instances of row classes and adds them to the DOM along with the send move button.
+ * Provides functions for minipulating the whole board.
+ *
+ */
 export class Game {
   private rows: Row[];
   private socket: SocketIOClient.Socket;
 
+  /**
+   *
+   * Creates an instance of Game. Adds the join button to the DOM.
+   *
+   * @param {IRow[]} rowsData
+   * @param {string} room
+   * @param {SocketIOClient.Socket} socket
+   * @memberof Game
+   *
+   */
   constructor(rowsData: IRow[], room: string, socket: SocketIOClient.Socket) {
     const rows: Row[] = [];
 
@@ -28,7 +45,6 @@ export class Game {
     sendMove.onclick = () => {
       for (let index = 0; index < this.rows.length; index += 1) {
         const row = this.rows[index];
-        console.log(row);
 
         if (row.selected.length > 0) {
           const move: IMove = {
@@ -36,8 +52,6 @@ export class Game {
             rowIndex: index,
             packetsRemoved: row.selected,
           };
-
-          console.log(move);
 
           this.broadcastMove(move);
           return;
@@ -47,7 +61,15 @@ export class Game {
     };
     rowsElm.appendChild(sendMove);
   }
-
+  /**
+   *
+   * Loops through all of the rows and disables them.
+   * Allows for disabling all but one row with the optional parameters.
+   *
+   * @param {number} [rowIgnored]
+   * @memberof Game
+   *
+   */
   public diableRows(rowIgnored?: number) {
     for (let index = 0; index < this.rows.length; index += 1) {
       if (index === rowIgnored) {
@@ -56,17 +78,37 @@ export class Game {
       this.rows[index].disable();
     }
   }
-
+  /**
+   *
+   * Enables all rows of the board
+   *
+   * @memberof Game
+   *
+   */
   public enableRows() {
     for (const row of this.rows) {
       row.enable();
     }
   }
-
+  /**
+   *
+   * Applies the move provided to the board deleting the packets that have been chosen.
+   *
+   * @param {IMove} move
+   * @memberof Game
+   *
+   */
   public applyMove(move: IMove) {
     this.rows[move.rowIndex].deletePackets(move.packetsRemoved);
   }
-
+  /**
+   *
+   * Applies the move to the board, disables all rows, and emits the move to the server.
+   *
+   * @param {IMove} move
+   * @memberof Game
+   *
+   */
   public broadcastMove(move: IMove) {
     this.applyMove(move);
     this.diableRows();
